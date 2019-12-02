613a9066aa6827a9c7d6e205f78102b7d67d26f8
plump_albert <plumpalbert@gmail.com>
2019-12-02 21:36:20 +0300

    Created tables

--- src/controllers/App.jsx
+++ src/controllers/App.jsx
@@ -4,26 +4,22 @@ import React from "react";
 import Product from "../components/Product";
 import Resource from "../components/Resource";
 
-const count = Math.ceil(Math.random() * 100);
-
-// let products = [
-//   { name: "Изделие № 1", production: 45.0, priority: 1 },
-//   { name: "Изделие № 2", production: 40.0, priority: 2 }
-// ];
-
 class App extends React.PureComponent {
   state = {
-    products: new Array(count).fill(0).map((v, i) => ({
-      name: `Изделие № ${i}`,
-      production: (Math.random() * 50).toFixed(2),
-      priority: Math.ceil(Math.random() * 3)
-    })),
-    resources: [],
+    products: [
+      { name: "Изделие № 1", production: 45.0, priority: 1 },
+      { name: "Изделие № 2", production: 40.0, priority: 2 }
+    ],
+    resources: [
+      { name: "Изделие № 1", production: 45.0, priority: 1 },
+      { name: "Изделие № 2", production: 40.0, priority: 2 }
+    ],
+    periods: 2,
     currentList: "products"
   };
 
   render() {
-    const { products, resources, currentList } = this.state;
+    const { products, resources, currentList, periods } = this.state;
     return (
       <div className="app">
         <div className="side-panel">
@@ -52,7 +48,108 @@ class App extends React.PureComponent {
             </div>
           </div>
         </div>
-        <div className="content"></div>
+        <div className="content">
+          <div className="table-wrapper">
+            <h2 className="content-header">
+              Затраты ресурсов на единицу продукции
+            </h2>
+            <table className="table table-consumption">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th colspan={resources.length}>Ресурсы</th>
+                </tr>
+                <tr>
+                  <th colspan={1}>Изделия</th>
+                  {resources.map((v, i) => (
+                    <th key={`consumption-header-${i}`}>{i + 1}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((row, i) => {
+                  return (
+                    <tr key={`consumption-row-${i}`}>
+                      <th>{i + 1}</th>
+                      {resources.map((col, j) => (
+                        <td key={`consumption-col-${j}`}>
+                          <input
+                            className="table-input"
+                            type="number"
+                            step="0.1"
+                            defaultValue={Math.random().toFixed(2)}
+                          />
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div className="table-wrapper">
+            <h2 className="content-header">Фонды ресурсов</h2>
+            <table className="table table-fonds">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th colspan={resources.length}>Ресурсы</th>
+                </tr>
+                <tr>
+                  <th>Период</th>
+                  {resources.map((v, i) => (
+                    <th key={`fonds-header-${i}`}>{i + 1}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {new Array(periods).fill(0).map((row, i) => (
+                  <tr key={`fonds-row-${i}`}>
+                    <th>{i + 1}</th>
+                    {resources.map((v, j) => (
+                      <td key={`fonds-col-${j}`}>
+                        <input
+                          className="table-input"
+                          type="number"
+                          step="0.1"
+                          defaultValue={Math.random().toFixed(2)}
+                        />
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="table-wrapper">
+            <h2 className="content-header">
+              Минимально необходимые партии продукции
+            </h2>
+            <table className="table parties-table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th colspan={products.length}>Изделия</th>
+                </tr>
+                <tr>
+                  <th>Период</th>
+                  {products.map((v, i) => (
+                    <th className={`parties-header-${i}`}>{i + 1}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {new Array(periods).fill(0).map((row, i) => (
+                  <tr key={`parties-row-${i}`}>
+                    {products.map((col, j) => (
+                      <td key={`parties-col-${j}`}></td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
